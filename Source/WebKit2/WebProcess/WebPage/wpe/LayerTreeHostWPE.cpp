c2b18cc1cc19322d7192246d36902378d8f2a49c
Zan Dobersek <zdobersek@igalia.com>
2015-03-02 09:57:16 -0800

    Revert "Revert to scheduling the flush in the frame callback in LayerTreeHostWPE. RPi drivers can't handle immediate flush."
    This reverts commit c0101540b2b8ff31e3a90f87fe714cecaa668bb4.

--- Source/WebKit2/WebProcess/WebPage/wpe/LayerTreeHostWPE.cpp
+++ Source/WebKit2/WebProcess/WebPage/wpe/LayerTreeHostWPE.cpp
@@ -397,7 +397,7 @@ const struct wl_callback_listener LayerTreeHostWPE::m_frameListener = {
         case FrameRequestState::ScheduleLayerFlushOnCompletion:
             ASSERT(!layerTreeHost.m_layerFlushTimer.isActive());
             layerTreeHost.m_frameRequestState = FrameRequestState::Completed;
-            layerTreeHost.scheduleLayerFlush();
+            layerTreeHost.flushAndRenderLayers();
             break;
         case FrameRequestState::Completed:
             ASSERT_NOT_REACHED();

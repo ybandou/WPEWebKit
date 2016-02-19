07c416019b16b77e227462a483478f12504de9f1
Zan Dobersek <zdobersek@igalia.com>
2016-02-19 09:21:23 -0800

    Add the wl_dispmanx protocol files.

---
+++ Source/WPE/Source/ViewBackend/Wayland/Protocols/wayland-dispmanx-protocol.c
@@ -0,0 +1,68 @@
+/* 
+ * Copyright © 2008-2011 Kristian Høgsberg
+ * Copyright © 2010-2011 Intel Corporation
+ * Copyright © 2013 Raspberry Pi Foundation
+ * 
+ * Permission to use, copy, modify, distribute, and sell this
+ * software and its documentation for any purpose is hereby granted
+ * without fee, provided that\n the above copyright notice appear in
+ * all copies and that both that copyright notice and this permission
+ * notice appear in supporting documentation, and that the name of
+ * the copyright holders not be used in advertising or publicity
+ * pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied
+ * warranty.
+ * 
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include "wayland-util.h"
+
+extern const struct wl_interface wl_buffer_interface;
+
+static const struct wl_interface *types[] = {
+	NULL,
+	&wl_buffer_interface,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	&wl_buffer_interface,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	&wl_buffer_interface,
+	NULL,
+};
+
+static const struct wl_message wl_dispmanx_requests[] = {
+	{ "create_buffer", "niiuuu", types + 1 },
+	{ "wrap_buffer", "nuiiuuu", types + 7 },
+};
+
+static const struct wl_message wl_dispmanx_events[] = {
+	{ "format", "u", types + 0 },
+	{ "buffer_allocated", "ou", types + 14 },
+};
+
+WL_EXPORT const struct wl_interface wl_dispmanx_interface = {
+	"wl_dispmanx", 1,
+	2, wl_dispmanx_requests,
+	2, wl_dispmanx_events,
+};
+

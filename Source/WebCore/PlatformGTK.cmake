e667823c62369bf009042c0eed6f4e4475033f8f
Xabier Rodriguez Calvar <calvaris@igalia.com>
2016-05-26 12:18:08 +0200

    [WebRTC] Moved GTK file to a common location

--- Source/WebCore/PlatformGTK.cmake
+++ Source/WebCore/PlatformGTK.cmake
@@ -149,7 +149,7 @@ list(APPEND WebCore_SOURCES
 
     platform/image-decoders/cairo/ImageDecoderCairo.cpp
 
-    platform/mediastream/gtk/SDPProcessorScriptResourceGtk.cpp
+    platform/mediastream/openwebrtc/SDPProcessorScriptResource.cpp
 
     platform/network/gtk/CredentialBackingStore.cpp
 
@@ -249,7 +249,7 @@ set(WebCore_USER_AGENT_SCRIPTS
 set(WebCore_USER_AGENT_SCRIPTS_DEPENDENCIES ${WEBCORE_DIR}/platform/gtk/RenderThemeGtk.cpp)
 
 set(WebCore_SDP_PROCESSOR_SCRIPTS ${WEBCORE_DIR}/Modules/mediastream/sdp.js)
-set(WebCore_SDP_PROCESSOR_SCRIPTS_DEPENDENCIES ${WEBCORE_DIR}/platform/mediastream/gtk/SDPProcessorScriptResource.cpp)
+set(WebCore_SDP_PROCESSOR_SCRIPTS_DEPENDENCIES ${WEBCORE_DIR}/platform/mediastream/openwebrtc/SDPProcessorScriptResource.cpp)
 
 list(APPEND WebCore_LIBRARIES
     ${ATK_LIBRARIES}

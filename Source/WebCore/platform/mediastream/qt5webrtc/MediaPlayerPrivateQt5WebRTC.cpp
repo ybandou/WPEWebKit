ab3388156307f427244b2f5b31568f3fbf212f38
Eugene Mutavchi <emutavchi@productengine.com>
2016-08-19 17:37:48 +0300

    Make it possible to render same stream to different video tags

--- Source/WebCore/platform/mediastream/qt5webrtc/MediaPlayerPrivateQt5WebRTC.cpp
+++ Source/WebCore/platform/mediastream/qt5webrtc/MediaPlayerPrivateQt5WebRTC.cpp
@@ -6,10 +6,40 @@
 #include <wtf/text/WTFString.h>
 #include <wtf/NeverDestroyed.h>
 
+#if USE(COORDINATED_GRAPHICS_THREADED)
+#include "TextureMapperGL.h"
+#include "TextureMapperPlatformLayerBuffer.h"
+#endif
+
+#include "BitmapImage.h"
+#include "BitmapTextureGL.h"
+#include "FloatRect.h"
+
+#include <cairo.h>
+
 #include "MediaPlayer.h"
 #include "NotImplemented.h"
 #include "TimeRanges.h"
 #include "MediaStreamPrivate.h"
+
+namespace {
+
+class ConditionNotifier {
+public:
+    ConditionNotifier(Lock& lock, Condition& condition)
+        : m_locker(lock), m_condition(condition)
+    {
+    }
+    ~ConditionNotifier()
+    {
+        m_condition.notifyOne();
+    }
+private:
+    LockHolder m_locker;
+    Condition& m_condition;
+};
+
+}  // namespace
 
 namespace WebCore {
 
@@ -35,10 +65,19 @@ void MediaPlayerPrivateQt5WebRTC::registerMediaEngine(MediaEngineRegistrar regis
 
 MediaPlayerPrivateQt5WebRTC::MediaPlayerPrivateQt5WebRTC(MediaPlayer* player)
     : m_player(player)
-{ }
+    , m_size(320, 240)
+{
+#if USE(COORDINATED_GRAPHICS_THREADED)
+    m_platformLayerProxy = adoptRef(new TextureMapperPlatformLayerProxy());
+    LockHolder locker(m_platformLayerProxy->lock());
+    m_platformLayerProxy->pushNextBuffer(
+        std::make_unique<TextureMapperPlatformLayerBuffer>(0, m_size, TextureMapperGL::ShouldOverwriteRect));
+#endif
+}
 
 MediaPlayerPrivateQt5WebRTC::~MediaPlayerPrivateQt5WebRTC()
 {
+    removeRenderer();
     m_player = 0;
 }
 
@@ -59,9 +98,7 @@ void MediaPlayerPrivateQt5WebRTC::setSize(const IntSize& size)
 {
     if (size == m_size)
         return;
-
     m_size = size;
-
     updateVideoRectangle();
 }
 
@@ -69,9 +106,7 @@ void MediaPlayerPrivateQt5WebRTC::setPosition(const IntPoint& position)
 {
     if (position == m_position)
         return;
-
     m_position = position;
-
     updateVideoRectangle();
 }
 
@@ -79,31 +114,148 @@ void MediaPlayerPrivateQt5WebRTC::play()
 {
     if (!m_stream || !m_stream->isProducingData())
         return;
-
     m_paused = false;
+    tryAttachRenderer();
+}
 
-    MediaStreamTrackPrivate *videoTrack = m_stream->activeVideoTrack();
-    if (videoTrack) {
-        RealtimeVideoSourceQt5WebRTC& videoSource = static_cast<RealtimeVideoSourceQt5WebRTC&>(videoTrack->source());
-        videoSource.startRenderer();
-        updateVideoRectangle();
-    }
+void MediaPlayerPrivateQt5WebRTC::pause()
+{
+    m_paused = true;
+    removeRenderer();
+}
+
+void MediaPlayerPrivateQt5WebRTC::setVisible(bool visible)
+{
+    if (visible)
+        tryAttachRenderer();
+    else
+        removeRenderer();
 }
 
 void MediaPlayerPrivateQt5WebRTC::updateVideoRectangle()
 {
+    if (m_rtcRenderer)
+        m_rtcRenderer->setVideoRectangle(m_position.x(), m_position.y(), m_size.width(), m_size.height());
+}
+
+void MediaPlayerPrivateQt5WebRTC::tryAttachRenderer()
+{
+    if (m_rtcRenderer)
+        return;
+
+    if (!m_stream || !m_stream->isProducingData())
+        return;
+
+    if (m_paused || !m_player->visible())
+        return;
+
+    MediaStreamTrackPrivate *videoTrack = m_stream->activeVideoTrack();
+    if (!videoTrack)
+        return;
+
+    RealtimeVideoSourceQt5WebRTC& videoSource = static_cast<RealtimeVideoSourceQt5WebRTC&>(videoTrack->source());
+    m_rtcRenderer.reset(getRTCMediaSourceCenter().createVideoRenderer(videoSource.rtcStream(), this));
+    videoSource.addObserver(this);
+
+    updateVideoRectangle();
+}
+
+void MediaPlayerPrivateQt5WebRTC::removeRenderer()
+{
+    if (!m_rtcRenderer)
+        return;
+
+    m_rtcRenderer.reset();
+
     MediaStreamTrackPrivate *videoTrack = m_stream ? m_stream->activeVideoTrack() : nullptr;
     if (videoTrack) {
         RealtimeVideoSourceQt5WebRTC& videoSource = static_cast<RealtimeVideoSourceQt5WebRTC&>(videoTrack->source());
-        videoSource.updateVideoRectangle(m_position.x(), m_position.y(), m_size.width(), m_size.height());
+        videoSource.removeObserver(this);
     }
 }
 
-PlatformLayer* MediaPlayerPrivateQt5WebRTC::platformLayer() const
+void MediaPlayerPrivateQt5WebRTC::renderFrame(const unsigned char *data, int byteCount, int width, int height)
 {
-    if (m_stream)
-        return m_stream->platformLayer();
-    return nullptr;
+#if USE(COORDINATED_GRAPHICS_THREADED)
+    cairo_format_t cairoFormat = CAIRO_FORMAT_ARGB32;
+    int stride = cairo_format_stride_for_width (cairoFormat, width);
+    ASSERT(byteCount >= (height * stride));
+    RefPtr<cairo_surface_t> surface = adoptRef(
+        cairo_image_surface_create_for_data(
+            (unsigned char*)data, cairoFormat, width, height, stride));
+    ASSERT(cairo_surface_status(surface.get()) == CAIRO_STATUS_SUCCESS);
+    RefPtr<BitmapImage> frame = BitmapImage::create(WTFMove(surface));
+    LockHolder lock(m_drawMutex);
+    bool succeeded = m_platformLayerProxy->scheduleUpdateOnCompositorThread([this, frame] {
+        ConditionNotifier notifier(m_drawMutex, m_drawCondition);
+        pushTextureToCompositor(frame);
+    });
+    if (succeeded) {
+        m_drawCondition.wait(m_drawMutex);
+    } else {
+        printf("***Error: scheduleUpdateOnCompositorThread failed\n");
+    }
+#endif
+}
+
+void MediaPlayerPrivateQt5WebRTC::punchHole(int width, int height)
+{
+#if USE(COORDINATED_GRAPHICS_THREADED)
+    LockHolder lock(m_drawMutex);
+    bool succeeded = m_platformLayerProxy->scheduleUpdateOnCompositorThread([this, width, height] {
+        ConditionNotifier notifier(m_drawMutex, m_drawCondition);
+        LockHolder holder(m_platformLayerProxy->lock());
+        m_platformLayerProxy->pushNextBuffer(
+            std::make_unique<TextureMapperPlatformLayerBuffer>(0, IntSize(width, height), TextureMapperGL::ShouldOverwriteRect));
+    });
+    if (succeeded) {
+        m_drawCondition.wait(m_drawMutex);
+    } else {
+        printf("***Error: scheduleUpdateOnCompositorThread failed\n");
+    }
+#endif
+}
+
+#if USE(COORDINATED_GRAPHICS_THREADED)
+void MediaPlayerPrivateQt5WebRTC::pushTextureToCompositor(RefPtr<Image> frame)
+{
+    LockHolder holder(m_platformLayerProxy->lock());
+    if (!m_platformLayerProxy->isActive()) {
+        printf("***Error: platformLayerProxy is not ready yet\n");
+        return;
+    }
+    IntSize size(frame->width(), frame->height());
+    std::unique_ptr<TextureMapperPlatformLayerBuffer> buffer =
+        m_platformLayerProxy->getAvailableBuffer(size, GraphicsContext3D::DONT_CARE);
+    if (UNLIKELY(!buffer)) {
+        if (UNLIKELY(!m_context3D))
+            m_context3D = GraphicsContext3D::create(
+                GraphicsContext3D::Attributes(), nullptr, GraphicsContext3D::RenderToCurrentGLContext);
+        RefPtr<BitmapTexture> texture = adoptRef(new BitmapTextureGL(m_context3D));
+        texture->reset(size, BitmapTexture::SupportsAlpha);
+        buffer = std::make_unique<TextureMapperPlatformLayerBuffer>(WTFMove(texture));
+    }
+    IntRect rect(IntPoint(),size);
+    buffer->textureGL().updateContents(
+        frame.get(), rect, /*offset*/ IntPoint(),
+        BitmapTexture::UpdateContentsFlag::UpdateCanModifyOriginalImageData);
+    m_platformLayerProxy->pushNextBuffer(WTFMove(buffer));
+}
+#endif
+
+void MediaPlayerPrivateQt5WebRTC::sourceStopped()
+{
+    removeRenderer();
+}
+
+void MediaPlayerPrivateQt5WebRTC::sourceMutedChanged()
+{
+    // Ignored
+}
+
+void MediaPlayerPrivateQt5WebRTC::sourceSettingsChanged()
+{
+    // Ignored
 }
 
 }
--- Source/WebCore/platform/mediastream/qt5webrtc/MediaPlayerPrivateQt5WebRTC.h
+++ Source/WebCore/platform/mediastream/qt5webrtc/MediaPlayerPrivateQt5WebRTC.h
@@ -5,9 +5,20 @@
 #include "IntRect.h"
 #include "FloatRect.h"
 
+#include "RealtimeMediaSourceCenterQt5WebRTC.h"
+
+#if USE(COORDINATED_GRAPHICS_THREADED)
+#include "TextureMapperPlatformLayerProxy.h"
+#endif
+
 namespace WebCore {
 
 class MediaPlayerPrivateQt5WebRTC : public MediaPlayerPrivateInterface
+    , public WRTCInt::RTCVideoRendererClient
+    , public RealtimeMediaSource::Observer
+#if USE(COORDINATED_GRAPHICS_THREADED)
+    , public TextureMapperPlatformLayerProxyProvider
+#endif
 {
 public:
     explicit MediaPlayerPrivateQt5WebRTC(MediaPlayer*);
@@ -29,7 +40,7 @@ public:
     void cancelLoad() override { }
 
     void play() override;
-    void pause() override { m_paused = true; }
+    void pause() override;
     bool paused() const override { return m_paused; }
     bool seeking() const override { return false; }
     std::unique_ptr<PlatformTimeRanges> buffered() const override { return std::make_unique<PlatformTimeRanges>(); }
@@ -38,7 +49,7 @@ public:
     float volume() const override { return 0; }
     bool supportsMuting() const override { return true; }
     void setMuted(bool) override { }
-    void setVisible(bool) override { }
+    void setVisible(bool) override;
     void setSize(const IntSize&) override;
     void setPosition(const IntPoint&) override;
     void paint(GraphicsContext&, const FloatRect&) override { }
@@ -48,13 +59,41 @@ public:
 
     FloatSize naturalSize() const override;
 
-    PlatformLayer* platformLayer() const override;
     bool supportsAcceleratedRendering() const override { return true; }
 
-private:
+#if USE(COORDINATED_GRAPHICS_THREADED)
+    PlatformLayer* platformLayer() const override { return const_cast<MediaPlayerPrivateQt5WebRTC*>(this); }
+    RefPtr<TextureMapperPlatformLayerProxy> proxy() const override { return m_platformLayerProxy.copyRef(); }
+    void swapBuffersIfNeeded() override { }
+#else
+    PlatformLayer* platformLayer() const override { return nullptr; }
+#endif
+
+    // RealtimeMediaSource::Observer
+    void sourceStopped() override;
+    void sourceMutedChanged() override;
+    void sourceSettingsChanged() override;
+    bool preventSourceFromStopping() override { return false; }
+
+    // WRTCInt::VideoPlayerClient
+    void renderFrame(const unsigned char *data, int byteCount, int width, int height) override;
+    void punchHole(int width, int height) override;
+
+  private:
     static void getSupportedTypes(HashSet<String, ASCIICaseInsensitiveHash>&);
     static MediaPlayer::SupportsType supportsType(const MediaEngineSupportParameters&);
     void updateVideoRectangle();
+    void tryAttachRenderer();
+    void removeRenderer();
+
+#if USE(COORDINATED_GRAPHICS_THREADED)
+    void pushTextureToCompositor(RefPtr<Image> frame);
+    RefPtr<TextureMapperPlatformLayerProxy> m_platformLayerProxy;
+    RefPtr<GraphicsContext3D> m_context3D;
+    Condition m_drawCondition;
+    Lock m_drawMutex;
+#endif
+    std::unique_ptr<WRTCInt::RTCVideoRenderer> m_rtcRenderer;
 
     MediaPlayer* m_player;
     IntSize m_size;

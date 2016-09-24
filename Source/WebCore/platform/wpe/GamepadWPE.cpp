ff0d5aa2c314e4653472aaf9053e3495e6b6b488
haseenamol@tataelxsi.co.in <haseenamol@tataelxsi.co.in>
2016-09-24 12:17:10 +0000

    Gamepad timestamp issue fix added

--- Source/WebCore/platform/wpe/GamepadWPE.cpp
+++ Source/WebCore/platform/wpe/GamepadWPE.cpp
@@ -1,3 +1,30 @@
+ /*
+ * Copyright (C) 2016 TATA ELXSI
+ * Copyright (C) 2016 Metrological
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "config.h"
 
 #if ENABLE(GAMEPAD)
@@ -21,6 +48,9 @@ GamepadWPE::GamepadWPE(String hidDevice, unsigned index)
     , m_valueMonitoringThread(0)
 {
     LOG(Gamepad, "%s(%s:%d)\n",__func__,__FILE__, __LINE__);
+
+    m_connectTime = m_lastUpdateTime = monotonicallyIncreasingTime();
+
     m_fileDescriptor=open(m_hidDevice.utf8().data(), O_RDONLY | O_NONBLOCK);
     if (m_fileDescriptor < 0)
 	return;
@@ -82,8 +112,9 @@ void GamepadWPE::updateForEvent(struct js_event event)
     }
     else if (event.type & JS_EVENT_BUTTON) {
        	m_buttonValues[event.number] = normalizeButtonValue(event.value);
-    }   
-    m_lastTimestamp = event.time;
+    }
+
+    m_lastUpdateTime = monotonicallyIncreasingTime();
 }
 
 void GamepadWPE::valueMonitoringThread(void* context)

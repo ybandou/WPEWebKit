DalavanCloud/WebKitForWayland

[EME] Remove keeping init datas for V2

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
@@ -407,10 +407,12 @@ bool MediaPlayerPrivateGStreamerBase::handleSyncMessage(GstMessage* message)
                 ++catenatedInitDataChunksNumber;
                 eventKeySystemIdString = eventKeySystemId;
                 if (g_strv_contains(streamEncryptionAllowedSystems, eventKeySystemId)) {
-                    GST_TRACE("keeping init data for %s", eventKeySystemId);
+                    GST_TRACE("considering init data handled for %s", eventKeySystemId);
+#if ENABLE(ENCRYPTED_MEDIA)
                     Vector<uint8_t> initDataVector;
                     initDataVector.append(reinterpret_cast<uint8_t*>(mapInfo.data), mapInfo.size);
                     m_initDatas.add(eventKeySystemIdString, WTFMove(initDataVector));
+#endif
                     m_handledProtectionEvents.add(GST_EVENT_SEQNUM(event.get()));
                 }
                 gst_buffer_unmap(data, &mapInfo);

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.h
@@ -276,9 +276,12 @@ private:
     Lock m_protectionMutex;
     Condition m_protectionCondition;
     String m_lastGenerateKeyRequestKeySystemUuid;
-    HashMap<String, Vector<uint8_t>> m_initDatas;
     HashSet<uint32_t> m_handledProtectionEvents;
-    void trimInitData(String keySystemUuid, const unsigned char*& initDataPtr, unsigned &initDataLength);
+#endif
+
+#if ENABLE(ENCRYPTED_MEDIA)
+    HashMap<String, Vector<uint8_t>> m_initDatas;
+    void trimInitData(String, const unsigned char*&, unsigned &);
 #endif
 };
 }

ybandou/WPEWebKit

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
@@ -431,10 +431,12 @@ bool MediaPlayerPrivateGStreamerBase::handleSyncMessage(GstMessage* message)
                 ++concatenatedInitDataChunksNumber;
                 eventKeySystemIdString = eventKeySystemId;
                 if (g_strv_contains(streamEncryptionAllowedSystems, eventKeySystemId)) {
-                    GST_TRACE("keeping init data for %s", eventKeySystemId);
+                    GST_TRACE("considering init data handled for %s", eventKeySystemId);
+#if ENABLE(LEGACY_ENCRYPTED_MEDIA_V1)
                     Vector<uint8_t> initDataVector;
                     initDataVector.append(reinterpret_cast<uint8_t*>(mapInfo.data), mapInfo.size);
                     m_initDatas.add(eventKeySystemIdString, WTFMove(initDataVector));
+#endif
                     m_handledProtectionEvents.add(GST_EVENT_SEQNUM(event.get()));
                 }
                 gst_buffer_unmap(data, &mapInfo);

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.h
@@ -290,9 +290,12 @@ private:
     Lock m_protectionMutex;
     Condition m_protectionCondition;
     String m_lastGenerateKeyRequestKeySystemUuid;
-    HashMap<String, Vector<uint8_t>> m_initDatas;
     HashSet<uint32_t> m_handledProtectionEvents;
-    void trimInitData(String keySystemUuid, const unsigned char*& initDataPtr, unsigned &initDataLength);
+#endif
+
+#if ENABLE(LEGACY_ENCRYPTED_MEDIA_V1)
+    HashMap<String, Vector<uint8_t>> m_initDatas;
+    void trimInitData(String, const unsigned char*&, unsigned &);
 #endif
 };
 
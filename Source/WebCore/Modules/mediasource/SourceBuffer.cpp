6b383076aed4789b9343d409c7ae7e41aac2e8b1
iivlev <iivlev@productengine.com>
2017-04-10 17:42:10 +0300

    [MSE][GStreamer] Optimizing MSE frame removal algorithm
    This patch is addressing microgap issue which is seen when
    erasedRanges are created in removeSamplesFromTrackBuffer.
    A consequence of the issue is significant performance
    downgrade, since PlatformTimeRanges::add and
    reverseFindSampleBeforePresentationTime
    have to iterate over all of the ranges in erasedRanges.
    After applying the patch, execution time of removeSamplesFromTrackBuffer
    dropped from 8+ seconds to < 300 ms.

--- Source/WebCore/Modules/mediasource/SourceBuffer.cpp
+++ Source/WebCore/Modules/mediasource/SourceBuffer.cpp
@@ -662,6 +662,9 @@ static PlatformTimeRanges removeSamplesFromTrackBuffer(const DecodeOrderSampleMa
     UNUSED_PARAM(buffer);
 #endif
 
+#if USE(GSTREAMER)
+    MediaTime microsecond = MediaTime::createWithDouble(0.000001);
+#endif
     PlatformTimeRanges erasedRanges;
     for (auto sampleIt : samples) {
         const DecodeOrderSampleMap::KeyType& decodeKey = sampleIt.first;
@@ -679,7 +682,11 @@ static PlatformTimeRanges removeSamplesFromTrackBuffer(const DecodeOrderSampleMa
         trackBuffer.decodeQueue.erase(decodeKey);
 
         auto startTime = sample->presentationTime();
+#if USE(GSTREAMER)
+        auto endTime = startTime + sample->duration() + microsecond;
+#else
         auto endTime = startTime + sample->duration();
+#endif
         erasedRanges.add(startTime, endTime);
 
 #if !LOG_DISABLED
